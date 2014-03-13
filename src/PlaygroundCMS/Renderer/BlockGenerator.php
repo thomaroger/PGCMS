5a8bcbfa6176f2b8aa39c12a93c96821a6beddf2
Thomas ROGER <thomaroger@gmail.com>
2014-03-13 23:46:11 +0100

    add sorts, filters & pagers for block list

---
+++ src/PlaygroundCMS/Renderer/BlockGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PlaygroundCms\Renderer;
+
+use Zend\ServiceManager\ServiceManagerAwareInterface;
+use Zend\ServiceManager\ServiceManager;
+use ZfcBase\EventManager\EventProvider;
+
+class BlockGenerator extends EventProvider implements ServiceManagerAwareInterface
+{
+   
+
+    public function generate($serviceManager, $block, $format = 'html', $parameters = array())
+    {
+        $controllerType = $block->getType();
+        $controller = new $controllerType($serviceManager, $block);
+
+        return $controller->renderAction($format, $parameters);
+    }
+
+   
+    /**
+     * Retrieve service manager instance
+     *
+     * @return ServiceManager
+     */
+    public function getServiceManager()
+    {
+        return $this->serviceManager;
+    }
+
+    /**
+     * Set service manager instance
+     *
+     * @param  ServiceManager $serviceManager
+     * @return User
+     */
+    public function setServiceManager(ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+
+        return $this;
+    }
+}
