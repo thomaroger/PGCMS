69f0154099c939182b4aad3f4f4eddf8c678195c
Thomas ROGER <thomaroger@gmail.com>
2014-05-08 01:41:11 +0200

    add phpdoc

--- src/PlaygroundCMS/Options/ModuleOptions.php
+++ src/PlaygroundCMS/Options/ModuleOptions.php
@@ -7,6 +7,7 @@
 * Classe d'options pour PlaygroundCMS 
 * Recuperation du templateMapResolver
 * Recuperation du translator
+* Recuperation des paths et url pour l'upload et la lecture des médias pour les templates et layouts
 **/
 
 namespace PlaygroundCMS\Options;
@@ -27,14 +28,34 @@ class ModuleOptions extends AbstractOptions
      */
     protected $translator;
 
-
+    /**
+     * @var string $themeFolder
+     */
     protected $themeFolder;
 
+    /**
+     * @var string $layout_path
+     */
     protected $layout_path = 'public/media/layout/';
+    
+    /**
+     * @var string $layout_url
+     */
     protected $layout_url  =  '/media/layout/';
 
+    /**
+     * @var string $template_path
+     */
     protected $template_path = 'public/media/template/';
+    
+    /**
+     * @var string $template_url
+     */
     protected $template_url  =  '/media/template/';
+    
+    /**
+     * @var Layout $currentLayout
+     */
     protected $currentLayout;
 
 
@@ -85,6 +106,12 @@ class ModuleOptions extends AbstractOptions
         return $this->translator;
     }
 
+    /**
+    * setThemeFolder : Setter pour le themeFolder
+    * @param String $themeFolder
+    *
+    * @return ModuleOptions $moduleOptions
+    */
     public function setThemeFolder($themeFolder)
     {
         $this->themeFolder = $themeFolder;
@@ -92,38 +119,78 @@ class ModuleOptions extends AbstractOptions
         return $this;
     }
 
-  
+    /**
+    * getThemeFolder : Getter pour le themeFolder
+    *
+    * @return String $themeFolder
+    */
     public function getThemeFolder()
     {
+
         return $this->themeFolder;
     }
 
+    /**
+    * getLayoutPath : Getter pour le layout_path
+    *
+    * @return String $layout_path
+    */
     public function getLayoutPath()
     {
+
         return $this->layout_path;
     }
 
+    /**
+    * getLayoutUrl : Getter pour le layout_url
+    *
+    * @return String $layout_url
+    */
     public function getLayoutUrl()
     {
+
         return $this->layout_url;
     }
 
+    /**
+    * getTemplatePath : Getter pour le template_path
+    *
+    * @return String $template_path
+    */
     public function getTemplatePath()
     {
+
         return $this->template_path;
     }
 
+    /**
+    * getTemplateUrl : Getter pour le template_url
+    *
+    * @return String $template_url
+    */
     public function getTemplateUrl()
     {
+
         return $this->template_url;
     }
 
-
+    /**
+    * getLayoutPath : Getter pour le layout_path
+    *
+    * @return String $currentLayout
+    */
     public function getCurrentLayout()
     {
+
         return $this->currentLayout;
     }
 
+    /**
+    * setThemeFolder : Setter pour le currentLayout
+    * @param String $currentLayout
+    *
+    * @return ModuleOptions $moduleOptions
+    */
     public function setCurrentLayout($currentLayout)
     {
         $this->currentLayout = $currentLayout;
