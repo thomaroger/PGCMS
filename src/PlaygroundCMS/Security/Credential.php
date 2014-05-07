a8f569249bba7d6fb80804cbaa548288c654fd7c
Thomas ROGER <thomaroger@gmail.com>
2014-05-08 01:55:49 +0200

    add phpdoc

--- src/PlaygroundCMS/Security/Credential.php
+++ src/PlaygroundCMS/Security/Credential.php
@@ -10,13 +10,34 @@ namespace PlaygroundCMS\Security;
 
 class Credential
 {
+    /**
+    * @var string SECURITY_ANONYMOUS
+    */
     const SECURITY_ANONYMOUS             = 'SECURITY_ANONYMOUS';
+    
+    /**
+    * @var string SECURITY_UNAUTHENTICATED
+    */
     const SECURITY_UNAUTHENTICATED       = 'SECURITY_UNAUTHENTICATED';
+    
+    /**
+    * @var string SECURITY_AUTHENTICATED
+    */
     const SECURITY_AUTHENTICATED         = 'SECURITY_AUTHENTICATED';
+    
+    /**
+    * @var string SECURITY_ACTIVATED_SUSCRIBER
+    */
     const SECURITY_ACTIVATED_SUSCRIBER   = 'SECURITY_ACTIVATED_SUSCRIBER';
+    
+    /**
+    * @var string SECURITY_UNACTIVATED_SUSCRIBER
+    */
     const SECURITY_UNACTIVATED_SUSCRIBER = 'SECURITY_UNACTIVATED_SUSCRIBER';
 
-
+    /**
+    * @var array statusesForm : liste des status pour prise en compte dans les forms (page)
+    */
     public static $statusesForm = array('success' => self::SECURITY_ANONYMOUS,
                                         'warning' => self::SECURITY_UNAUTHENTICATED,
                                         'danger' => self::SECURITY_AUTHENTICATED,
