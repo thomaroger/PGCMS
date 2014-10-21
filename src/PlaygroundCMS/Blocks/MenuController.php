e49bab91c83e894706a37ee3099254c548b7106b
Thomas ROGER <thomaroger@gmail.com>
2014-10-21 02:12:25 +0200

    add block menu for front

--- src/PlaygroundCMS/Blocks/MenuController.php
+++ src/PlaygroundCMS/Blocks/MenuController.php
@@ -12,19 +12,69 @@ namespace PlaygroundCMS\Blocks;
 
 use Zend\View\Model\ViewModel;
 
-class MEnuController extends AbstractBlockController
+class MenuController extends AbstractBlockController
 {
+    protected $menuService;
     /**
     * {@inheritdoc}
     * renderBlock : Rendu du bloc d'un bloc HTML
     */
     protected function renderBlock()
     {
+        $params = array();
         $block = $this->getBlock();
-        $params = array('block' => $block);
+
+        $menuId = $block->getParam('menu');
+
+        $menu = $this->getMenuService()->getMenuMapper()->findById($menuId);
+
+        $repository = $this->getMenuService()->getMenuMapper()->getEntityRepository();
+        
+        $controller = $this;
+        $options = array(
+            'decorate' => true,
+            'rootOpen' => '<ul class="nav navbar-nav">',
+            'rootClose' => '</ul>',
+            'childOpen' => '',
+            'childClose' => '',
+            'nodeDecorator' => function($node) use (&$controller) {
+                return $controller->decorateNode($node);
+            }
+        );
+
+        $params['htmlTree'] = $repository->childrenHierarchy($menu, false, $options);
+
+
         $model = new ViewModel($params);
         
         return $this->render($model);
+    }
+
+    public function decorateNode($node)
+    {
+        $menu = $this->getMenuService()->getMenuMapper()->findById($node['id']);
+        $menu->setTranslatableLocale($this->getRessource()->getLocale());
+        $this->getMenuService()->getMenuMapper()->getEntityManager()->refresh($menu); 
+
+        $html = "";
+        
+        if($menu->getChildren()->count() > 0) { 
+            $html .= '<li class="dropdown">';
+            $html .= '<a href="#" class="dropdown-toggle" data-toggle="dropdown">'.$menu->getTitle().' <span class="caret"></span></a>';
+            $html .= '<ul class="dropdown-menu" role="menu">';
+            foreach ($menu->getChildren() as $submenu) {
+                $html .= '<li class=""><a href="'.$submenu->getUrl().'">'.$submenu->getTitle().'</a></li>';
+            }
+            $html .= '</ul>';
+            $html .= '</li>';
+        }else {
+            if($menu->getLevel() <= 2) {
+                $html .= '<li class=""><a href="'.$menu->getUrl().'">'.$menu->getTitle().'</a></li>';
+            }
+        }
+
+        
+        return $html; 
     }
 
    
@@ -44,5 +94,31 @@ class MEnuController extends AbstractBlockController
     {
         
         return 'Block Menu';
+    }
+
+    /**
+     * getFeedService : Recuperation du service de feed
+     *
+     * @return Feed $feedService : feedService
+     */
+    private function getMenuService()
+    {
+        if (null === $this->menuService) {
+            $this->setMenuService($this->getServiceManager()->get('playgroundcms_menu_service'));
+        }
+
+        return $this->menuService;
+    }
+    /**
+     * setFeedService : Setter du service de feed
+     * @param  Feed $feedService
+     *
+     * @return FeedController $this
+     */
+    private function setMenuService($menuService)
+    {
+        $this->menuService = $menuService;
+
+        return $this;
     }
 }
